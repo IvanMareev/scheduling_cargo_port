8171ec5e7931dd8ecf2c3677f18c9fd6feb9b95c
ivanMareev <rain12003@yandex.ru>
2025-03-29 20:37:13 +1000

    wip

---
+++ scheduling_cargo_port/src/pages/MainPages/MainPages.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { FormControl } from '@mui/material';
+
+function MainPages() {
+
+
+    return (
+        <>
+            fdfd
+            <FormControl>
+                <InputLabel htmlFor="my-input">Email address</InputLabel>
+                <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+            </FormControl>
+        </>
+    )
+}
+
+export default MainPages
